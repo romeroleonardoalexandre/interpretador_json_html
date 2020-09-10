de21f444dc733222174162c806b2c28b06bc314f
Alexandre Leonardo Romero <alexandre.romero@linx.com.br>
2020-09-10 12:35:49 -0300

    Modulo de interpretador para react

---
+++ index.js
@@ -0,0 +1,8 @@
+import { React } from './modules/react.js';
+
+// create the canvas and reporting list
+let react = new React(html, document.body, 480, 320);
+myCanvas.create();
+myCanvas.createReportList();
+
+
