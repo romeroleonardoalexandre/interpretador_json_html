b5a18114238422ec7edf6f77d8fa34778c662156
Alexandre Leonardo Romero <alexandre.romero@linx.com.br>
2020-10-20 13:02:11 -0300

    ajustes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // create the canvas and reporting list
 export default function initInterpretador(objeto = 'react', json_data = {}){
 	if(objeto == 'react'){
-		import React from './modules/react.js';
+		import React from './modules/react.js'
 		let Interpretador = new React(json_data)
 	}
 
