5c65ce96b86ae23a554c46f558a8b8ef5985f05f
Alexandre Leonardo Romero <alexandre.romero@linx.com.br>
2020-10-20 15:12:10 -0300

    exemplo html

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
 // create the canvas and reporting list
+import React from './react.js'
+
 export default function initInterpretador(objeto = 'react', json_data = {}){
 	if(objeto == 'react'){
-		import React from './modules/react.js'
 		let Interpretador = new React(json_data)
 	}
 
